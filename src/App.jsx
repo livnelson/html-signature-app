6823482a7c42f9d7fe1ca40a3d4f4bdaf202b410
livnelson <livnelson@me.com>
2024-03-24 14:02:24 -0700

    converted to download file instead of copy

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
       <h1>Email Signature Generator</h1>
-      <p className="instructions">To use this email generator, fill in the fields below and click the "Copy HTML Signature Button", then paste the code into your email signature provider.</p>
+      <p className="instructions">To use this email generator, fill in the fields below and click the "Download HTML Signature Button", then rightclick on the file and paste it into your email signature provider.</p>
       <EmailSignatureForm />
     </div>
   )
--- src/components/EmailSignatureForm.jsx
+++ src/components/EmailSignatureForm.jsx
@@ -7,6 +7,7 @@ const EmailSignatureForm = () => {
   const [cellPhone, setCellPhone] = useState('')
   const [email, setEmail] = useState('')
   const [photoURL, setPhotoURL] = useState('')
+  const [error, setError] = useState('')
 
   const sanitizeAndEscape = (input) => {
     const sanitizedInput = DOMPurify.sanitize(input)
@@ -16,6 +17,18 @@ const EmailSignatureForm = () => {
       .replace(/>/g, '&gt;')
       .replace(/"/g, '&quot;')
       .replace(/'/g, '&#39;')
+  }
+
+  const isValidEmail = (email) => {
+    // Simple email validation using regex
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return emailPattern.test(email)
+  }
+
+  const isValidURL = (url) => {
+    // Simple URL validation using regex
+    const urlPattern = /^(https?:\/\/)?([\w\d]+\.)?[\w\d]+\.[\w\d]{2,}$/i
+    return urlPattern.test(url)
   }
 
   const generateSignature = () => {
@@ -34,36 +47,26 @@ const EmailSignatureForm = () => {
         <title>Email Signature</title>
       </head>
       <body style="font-family: Arial, sans-serif">
-        <table cellpadding="0" cellspacing="0" style="width: auto; height: 121px;">
+        <table cellpadding="0" cellspacing="0" style="width: 300px;">
           <tr>
             <td style="padding: 4px 10px; border-right: 1px solid #ccc">
-              <img
-                src="${sanitizedProfileImg}"
-                alt="Profile Photo"
-                style="width: 90px; height: 90px;"
-              />
-              <h5 style="margin: 3px 0px 0px 0px; color: #ef8022; font-size: 13px;">${sanitizedName}</h5>
-                <p style="margin: 0; font-size: 10px"><strong>${sanitizedTitle}</strong></p>
+              <img src="https://res.cloudinary.com/dovuffpii/image/upload/v1711154514/Peoplescape/belinda_uuekxk.jpg" alt="Profile Photo" width="90" height="90" />
+              <h5 style="margin: 3px 0px; color: #ef8022; font-size: 13px;">Belinda Morris</h5>
+              <p style="margin: 0; font-size: 10px"><strong>CEO &amp; Founder</strong></p>
             </td>
             <td style="padding: 0px 10px">
               <p style="margin: 0px; font-size: 12px">
                 <span style="color:#ef8022;"><strong>M:</strong></span>
-                <a href="tel:${sanitizedCellPhone}" style="text-decoration: none; color: #000; cursor:pointer;">${sanitizedCellPhone}</a>
+                <a href="tel:760-880-7005" style="text-decoration: none; color: #000; cursor:pointer;">760-880-7005</a>
               </p>
               <p style="margin: 6px 0px; font-size: 12px">
                 <span style="color:#ef8022;"><strong>O:</strong></span>
-                <a href="tel: 3239000511" style="text-decoration: none; color: #000; cursor:pointer;">323-900-0511</a>
+                <a href="tel:3239000511" style="text-decoration: none; color: #000; cursor:pointer;">323-900-0511</a>
               </p>
               <p style="margin: 4px 0px; font-size: 12px">
-                <a href="mailto:${sanitizedEmail}" style="text-decoration: none; color: #000; cursor:pointer;"
-                  >${sanitizedEmail}</a
-                >
+                <a href="mailto:belinda@peoplescapehr.com" style="text-decoration: none; color: #000; cursor:pointer;">belinda@peoplescapehr.com</a>
               </p>
-              <img
-                src="https://res.cloudinary.com/dovuffpii/image/upload/v1711154850/Peoplescape/peoplescape-hr-logo_figp7x.png"
-                alt="Peoplescape HR Logo"
-                style="width: 150px; margin-top: 4px;"
-              />
+              <img src="https://res.cloudinary.com/dovuffpii/image/upload/v1711154850/Peoplescape/peoplescape-hr-logo_figp7x.png" alt="Peoplescape HR Logo" width="150" height="auto" />
             </td>
           </tr>
         </table>
@@ -73,12 +76,23 @@ const EmailSignatureForm = () => {
     return signature
   }
 
-  const handleCopySignature = () => {
-    const signature = generateSignature()
-    navigator.clipboard
-      .writeText(signature)
-      .then(() => alert('HTML signature copied to clipboard!'))
-      .catch((error) => console.error('Clipboard copy error:', error))
+  const handleDownloadSignature = () => {
+    if (!name || !title || !cellPhone || !email || !photoURL) {
+      setError('Please fill in all fields.')
+    } else if (!isValidEmail(email)) {
+      setError('Please enter a valid email address.')
+    } else if (!isValidURL(photoURL)) {
+      setError('Please enter a valid URL for the Profile Photo.')
+    } else {
+      const signature = generateSignature()
+      const blob = new Blob([signature], { type: 'text/html' })
+      const url = URL.createObjectURL(blob)
+      const a = document.createElement('a')
+      a.href = url
+      a.download = `${sanitizeAndEscape(name)}_signature.html`
+      a.click()
+      setError('')
+    }
   }
 
   return (
@@ -98,7 +112,8 @@ const EmailSignatureForm = () => {
       <label>Profile Photo URL:</label>
       <input type='url' value={photoURL} required onChange={(e) => setPhotoURL(e.target.value)} />
       <br />
-      <button onClick={handleCopySignature}>Copy HTML Signature</button>
+      <button onClick={handleDownloadSignature}>Download HTML Signature</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
   )
 }
