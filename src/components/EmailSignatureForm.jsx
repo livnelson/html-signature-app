8c1bf03baf619e159ed3bc5ecf30dd2baf1434ea
livnelson <livnelson@me.com>
2024-04-12 18:41:18 -0700

    tweaked layout

--- src/components/EmailSignatureForm.jsx
+++ src/components/EmailSignatureForm.jsx
@@ -39,53 +39,51 @@ const EmailSignatureForm = () => {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <style>
-          .signature-table {
-            width: 100%;
-            max-width: 500px; /* Adjust max-width as needed */
-            border-collapse: collapse;
-          }
-          .signature-table td {
-            padding: 6px;
-            vertical-align: top;
-          }
-          .profile-cell {
-            padding-right: 0; /* Remove right padding for profile image cell */
-            border-right: 1px solid #ccc;
-            width: 110px; /* Adjust width of profile image column */
-          }
-          .profile-cell img {
-            display: block;
-            width: 100%; /* Ensure profile image fills the column */
-            height: auto;
-            margin-bottom: 6px;
-          }
-        </style>
+        <meta
+          http-equiv="X-UA-Compatible"
+          content="IE=edge"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
         <title>HTML Email Signature</title>
       </head>
-      <body style="margin: 0; padding: 0">
-        <p style="margin: 0">Warm Regards,</p>
-        <p style="margin: 10px 0px">${sanitizedFirstName}</p>
+      <body style="margin: 0; padding: 0; font-family: Calibri, sans-serif">
+        <p style="margin: 0; font-size: 12px">Warm Regards,</p>
+        <p style="margin: 10px 0px; font-size: 12px">${sanitizedFirstName}</p>
         <table
-          class="signature-table"
           cellpadding="0"
           cellspacing="0"
-          style="width: 100%; margin-top: 6px"
+          style="
+            width: 100%;
+            max-width: 500px;
+            margin-top: 6px;
+            border-collapse: collapse;
+            font-family: Calibri, sans-serif;
+            font-size: 11px;
+          "
         >
           <tr>
-            <td class="profile-cell">
+            <td
+              style="
+                padding: 6px;
+                vertical-align: top;
+                width: 110px;
+                max-width: 120px;
+                border-right: 1px solid #ccc;
+              "
+            >
               <a
                 href="https://peoplescapehr.com/our-team/"
                 target="_blank"
                 style="text-decoration: none"
               >
-                <img
-                  src="${sanitizedProfileImg}"
-                  alt="Profile Photo"
-                  width="100"
-                  height="100"
+                <img 
+                  src="${sanitizedProfileImg}" 
+                  alt="Profile Photo" 
+                  width="100" 
+                  height="100" 
                   style="margin-right: 6px"
                 />
                 <h5
@@ -98,13 +96,13 @@ const EmailSignatureForm = () => {
                 >
                   ${sanitizedFirstName} ${sanitizedLastName}
                 </h5>
-                <p style="margin: 0; font-size: 10px">
+                <p style="margin: 0; line-height: 1.25">
                   <strong>${sanitizedTitle}</strong>
                 </p>
               </a>
             </td>
-            <td>
-              <p style="margin: 0; margin-bottom: 2px; padding-bottom: 3px; font-size: 12px;">
+            <td style="padding: 6px; padding-left: 10px; vertical-align: top">
+              <p style="margin: 0; margin-bottom: 2px; padding-bottom: 3px">
                 <span style="color: #ef8022"><strong>M:</strong></span>
                 <a
                   href="tel:${sanitizedCellPhone}"
@@ -112,22 +110,25 @@ const EmailSignatureForm = () => {
                   >${sanitizedCellPhone}</a
                 >
               </p>
-              <p style="margin: 0; margin-bottom: 2px; padding-bottom: 3px;  font-size: 12px">
+              <p style="margin: 0; margin-bottom: 2px; padding-bottom: 3px">
                 <span style="color: #ef8022"><strong>O:</strong></span>
                 <a
-                  href="tel:760-880-7005"
+                  href="tel:323 900-0511"
                   style="text-decoration: none; color: #000"
-                  >760-880-7005</a
+                  >323 900-0511</a
                 >
               </p>
-              <p style="margin: 0; margin-bottom: 2px; padding-bottom: 3px; font-size: 12px">
+              <p style="margin: 0; margin-bottom: 2px; padding-bottom: 3px">
                 <a
                   href="mailto:${sanitizedEmail}"
                   style="text-decoration: none; color: #000"
                   >${sanitizedEmail}</a
                 >
               </p>
-              <a href="https://peoplescapehr.com/" target="_blank">
+              <a
+                href="https://peoplescapehr.com/"
+                target="_blank"
+              >
                 <img
                   src="https://peoplescapehr.com/wp-content/uploads/2020/07/peoplescape_logo_2020-e1594693165758.png"
                   alt="Peoplescape HR Logo"
@@ -139,7 +140,13 @@ const EmailSignatureForm = () => {
             </td>
           </tr>
         </table>
-        <p style="font-size: 12px; margin: 10px 6px 0">
+        <p
+          style="
+            font-family: Calibri, sans-serif;
+            font-size: 11px;
+            margin: 10px 6px 0;
+          "
+        >
           <strong
             >I respect your personal time and do not expect a response when you are
             not at work.</strong
@@ -147,13 +154,20 @@ const EmailSignatureForm = () => {
         </p>
       </body>
     </html>
-        `
+            `
     return signature
   }
 
   const handleDownloadSignature = (e) => {
     e.preventDefault()
-    if (!firstName || !lastName || !title || !cellPhone || !email || !photoURL) {
+    if (
+      !firstName ||
+      !lastName ||
+      !title ||
+      !cellPhone ||
+      !email ||
+      !photoURL
+    ) {
       setError('Please fill in all fields.')
     } else if (!isValidEmail(email)) {
       setError('Please enter a valid email address.')
@@ -231,7 +245,9 @@ const EmailSignatureForm = () => {
           onChange={(e) => setPhotoURL(e.target.value)}
         />
         <br />
-        <button onClick={handleDownloadSignature}>Download HTML Signature</button>
+        <button onClick={handleDownloadSignature}>
+          Download HTML Signature
+        </button>
         {error && <p style={{ color: 'red' }}>{error}</p>}
       </form>
     </div>
